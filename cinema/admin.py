536b228c3a895fc8919dc802b60e10a94e42207d
e.Lyalkov <e_lyalkov@mail.ru>
2025-06-13 11:21:23 +0300

    Зарегистрировал модели в admin.py

--- cinema/admin.py
+++ cinema/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from cinema.models import Genre, Director, Actors, Movie, CinemaHall, Session, Booking
 
-# Register your models here.
+
+admin.site.register(Genre)
+admin.site.register(Director)
+admin.site.register(Actors)
+admin.site.register(Movie)
+admin.site.register(CinemaHall)
+admin.site.register(Session)
+admin.site.register(Booking)
