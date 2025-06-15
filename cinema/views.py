67828d684ebd1986e0e6f4d0b1a0913a70b01237
e.lyalkov <e.lyalkov@gmail.com>
2025-06-15 15:39:22 +0300

    добавил test_logging, написал первое представление

--- cinema/views.py
+++ cinema/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+import logging
+from cinema.models import Movie
 
-# Create your views here.
+logger = logging.getLogger(__name__)
+
+
+def home(request):
+    movies = Movie.objects.all().order_by('-retease_date')
+
+
+    context = {
+        'movies': movies,
+    }
+    logger.info('INFO0000000000')
+    return render(request, 'cinema/home.html', context)
+
+
